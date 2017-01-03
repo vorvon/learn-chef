fea77e62127897ad19aa3ac9c551fa0324ad8d03
Joel <vorvon@gmail.com>
2017-01-03 14:02:04 -0800

    learn-chef-cookbook

---
+++ cookbooks/learn_chef_apache2/recipes/default.rb
@@ -0,0 +1,19 @@
+apt_update 'Update the apt cache daily' do
+  frequency 86_400
+  action :periodic
+end
+
+package 'apache2'
+
+service 'apache2' do
+  supports :status => true
+  action [:enable, :start]
+end
+
+template '/var/www/html/index.html' do
+  source 'index.html.erb'
+end#
+# Cookbook Name:: learn_chef_apache2
+# Recipe:: default
+#
+# Copyright (c) 2017 The Authors, All Rights Reserved.
---
+++ webserver.rb
@@ -0,0 +1,22 @@
+apt_update 'Update the apt cache daily' do
+	frequency 86_400
+	action :periodic
+end
+
+
+package 'apache2'
+
+
+service 'apache2' do
+	supports :status => true
+	action [:enable, :start]
+end
+
+
+file '/var/www/html/index.html' do
+	content '<html>
+		<body>
+			<h1>Hello World</h1>
+		</body>
+	</html>'
+end
